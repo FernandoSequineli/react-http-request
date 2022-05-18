a382a6f3acc5a9f0bba5468fab6271294a73a254
Fernando Sequineli <sequineli.fernando@gmail.com>
2022-05-18 12:11:17 +0100

    installed json server and running the first test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import "./App.css";
 
+import { useState, useEffect } from "react";
+
+const url = "http://localhost:3000/products";
+
 function App() {
-  return <h1>Hi</h1>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Products List</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - ${product.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
