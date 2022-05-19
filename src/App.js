92310874cd65c36d5a5fd90e489457a3c5ceb0f4
Fernando Sequineli <sequineli.fernando@gmail.com>
2022-05-19 10:51:33 +0100

    added function to create new product based on information from form and display the product dynamically

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ const url = "http://localhost:3000/products";
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -18,16 +20,63 @@ function App() {
     fetchData();
   }, []);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price,
+    };
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    });
+
+    const addedProduct = await res.json();
+
+    setProducts((prev) => [...prev, addedProduct]);
+
+    setName("");
+    setPrice("");
+  };
+
   return (
     <div className="App">
       <h1>Products List</h1>
-      <ul>
+      <ul className="product-list">
         {products.map((product) => (
           <li key={product.id}>
             {product.name} - ${product.price}
           </li>
         ))}
       </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Name:
+            <input
+              type="text"
+              value={name}
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            Price:
+            <input
+              type="number"
+              value={price}
+              name="price"
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+          <input type="submit" value="Create" />
+        </form>
+      </div>
     </div>
   );
 }
